fa32e773fa2eed96e641eb77fc1a42a1210f4ebe
vicying <windincns@gmil.com>
2018-11-29 16:27:36 +0800

    no message

--- install/other/VirBOX.sh
+++ install/other/VirBOX.sh
@@ -2,13 +2,16 @@ yum -y install epel-release
 rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 rpm -Uvh http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
-yum update
+yum -y update
 
 yum -y install bzip2 kernel-devel kernel-headers gcc make dkms
    
 mkdir /media/cdrom
 
 mount /dev/cdrom /media/cdrom/
+
+
+
 
 /media/cdrom/VBoxLinuxAdditions.run
 
