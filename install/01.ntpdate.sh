76f1e7740a2af2f71e653b26261ac491f008f90b
vicying <windincns@gmil.com>
2019-01-09 16:41:47 +0800

    no message

--- install/01.ntpdate.sh
+++ install/01.ntpdate.sh
@@ -4,6 +4,7 @@
 ##
 
 yum -y install ntpdate
+timedatectl set-timezone Asia/Taipei
 echo "# 每日校時" >> /var/spool/cron/root
 echo "10 5 * * * /usr/sbin/ntpdate tock.stdtime.gov.tw" >> /var/spool/cron/root
 echo "" >> /var/spool/cron/root
